ac7e2242ac26ad9120aa6714aba0b023533ad930
Giulia Alfonsi <electric.g@gmail.com>
2023-03-17 13:10:31 -0700

    Test 7

--- js/settings.js
+++ js/settings.js
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: "off" */
 'use strict';
 
-const VERSION = '0.7.9';
+const VERSION = '0.7.10';
 const NAMESPACE = 'mobiFavStations';
 
 const URL_LOCAL = 'http://localhost:8080/mobi-fav-stations/json/';
