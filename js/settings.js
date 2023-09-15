7aaba9f905bd07aae8837f15e96c0d2d5b925385
Giulia Alfonsi <electric.g@gmail.com>
2023-09-15 11:21:25 -0700

    Temporarily disabled subgrids because of chrome

--- js/settings.js
+++ js/settings.js
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: "off" */
 'use strict';
 
-const VERSION = '0.7.25';
+const VERSION = '0.7.26';
 const NAMESPACE = 'mobiFavStations';
 
 const URL_LOCAL = 'http://localhost:8080/mobi-fav-stations/json/';
