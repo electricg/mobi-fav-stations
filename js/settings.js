6686771834fe2c117716ff34eb8a5c958bcf350a
Giulia Alfonsi <electric.g@gmail.com>
2025-02-06 07:52:11 -0800

    Small update

--- js/settings.js
+++ js/settings.js
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: "off" */
 'use strict';
 
-const VERSION = '0.8.8';
+const VERSION = '0.8.9';
 const NAMESPACE = 'mobiFavStations';
 
 const URL_LOCAL = 'http://localhost:8080/mobi-fav-stations/json/';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '0.8.8';
+const VERSION = '0.8.9';
 const cacheName = `v${VERSION}::static`;
 
 const fileList = `
