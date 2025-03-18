a593688686447b6bd24921c6581cc96b4eb09981
Giulia Alfonsi <electric.g@gmail.com>
2025-03-17 19:22:21 -0700

    Preload bike status

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 /* eslint no-unused-vars: "off" */
 'use strict';
 
-const VERSION = '0.8.41';
+const VERSION = '0.8.42';
 const NAMESPACE = 'mobiFavStations';
 
 const URL_LOCAL = 'http://localhost:8080/mobi-fav-stations/json/';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '0.8.41';
+const VERSION = '0.8.42';
 const NAMESPACE = 'mobiFavStations';
 
 const CACHE_NAMESPACE = `::${NAMESPACE}::`;
