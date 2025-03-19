2be1fc00b1a53ebe61653f50e08e78e32f5b7adc
Giulia Alfonsi <electric.g@gmail.com>
2025-03-19 12:11:36 -0700

    Better docs and functions for localstorage lib

--- js/storage.js
+++ js/storage.js
@@ -2,7 +2,12 @@
   'use strict';
 
   const Storage = function (namespace) {
-    const capitalize = function (string) {
+    /**
+     * Transform to uppercase the first letter of the string
+     * @param {string} string String to transform
+     * @returns {string} Transformed string
+     */
+    const capitalize = (string) => {
       if (string === '') {
         return string;
       }
@@ -10,12 +15,20 @@
     };
 
     /**
-     * Load from localStorage
-     * @returns {object}
+     * Get key with project namespace
+     * @param {string} key Key to get
+     * @returns {string} Key with namespace
+     */
+    const getKey = (key) => namespace + capitalize(key);
+
+    /**
+     * Load value from localStorage
+     * @param {string} key Key to get
+     * @returns {object} Value
      */
     this.getItem = function (key) {
       try {
-        return JSON.parse(localStorage.getItem(namespace + capitalize(key)));
+        return JSON.parse(localStorage.getItem(getKey(key)));
       } catch (e) {
         console.error(e);
         return {};
@@ -24,11 +37,13 @@
 
     /**
      * Save to localStorage
+     * @param {string} key
+     * @param {object} data
      * @returns {boolean} True if save was successful
      */
     this.setItem = function (key, data) {
       try {
-        localStorage.setItem(namespace + capitalize(key), JSON.stringify(data));
+        localStorage.setItem(getKey(key), JSON.stringify(data));
         return true;
       } catch (e) {
         console.error(e);
@@ -38,11 +53,12 @@
 
     /**
      * Remove item from localStorage
-     * @returns {object}
+     * @param {string} key Key to remove
+     * @returns {object|boolean} Removed value or false if unsuccessful
      */
     this.removeItem = function (key) {
       try {
-        return localStorage.removeItem(namespace + capitalize(key));
+        return localStorage.removeItem(getKey(key));
       } catch (e) {
         console.error(e);
         return false;
@@ -50,7 +66,7 @@
     };
 
     /**
-     * Clear all items of the namespace
+     * Clear all items of the project namespace
      */
     this.clear = function () {
       Object.keys(localStorage).forEach((key) => {
